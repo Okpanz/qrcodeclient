00349549dad49ff64a667a47490835b4de0bfcbb
Okpanz <okpanz700@gmail.com>
2024-04-12 13:08:46 +0100

    details verification added to the input fields

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -58,14 +58,14 @@ const Login = () => {
       <div className="md:w-1/2 p-8 flex items-center justify-center">
         <form onSubmit={handleSubmit} className="bg-white shadow-md rounded-md p-6 w-full max-w-md">
           <h2 className="text-2xl font-semibold text-center mb-6">Login</h2>
-          {error && <div className="text-red-500 mb-4">{error}</div>} {/* Display error message if exists */}
+          {error && <div className="text-red-500 mb-4">{error}</div>}
           <div className="mb-4">
             <label htmlFor="email" className="block text-gray-700 font-medium mb-2">Email</label>
-            <input type="text" name="email" value={credentials.email} onChange={handleChange} className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500" />
+            <input type="text" name="email" value={credentials.email} onChange={handleChange} className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500" required/>
           </div>
           <div className="mb-4">
             <label htmlFor="password" className="block text-gray-700 font-medium mb-2">Password</label>
-            <input type="password" name="password" value={credentials.password} onChange={handleChange} className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500" />
+            <input type="password" name="password" value={credentials.password} onChange={handleChange} className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500" required/>
           </div>
           <button type="submit" className="w-full bg-primary text-white py-2 rounded-md hover:bg-secondary hover:text-primary transition-all ease-in-out duration-200 focus:outline-none focus:bg-primary">
             {isLoading ? 'Logging in...' : 'Login'} {/* Display different text based on loading state */}
